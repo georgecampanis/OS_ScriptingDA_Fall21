9d079c85e8d5e5984864e1baf688c46eea98ebb5
George Campanis <georgecampanis@gmail.com>
2021-11-23 13:18:08 +0000

    Add files via upload

---
+++ SimpleMenuExample.ps1
@@ -0,0 +1,38 @@
+﻿
+
+
+function createUser(){
+Clear-Host;
+Write-Host "Createing User...";
+
+}
+
+Clear-Host;
+
+Do {
+
+
+Write-Host " ***************************"
+Write-Host " *           Menu          *"
+Write-Host " ***************************"
+Write-Host
+Write-Host " 1. Create User"
+Write-Host " 2. Create Group"
+Write-Host " 3. Add User to Group"
+Write-Host " 4. Change Password"
+Write-Host " 5. Quit"
+Write-Host
+Write-Host " Select an option and press Enter: " -nonewline
+
+$Select = Read-Host
+Switch ($Select)
+    {
+    1 { createUser}
+    2 { Commands to be run if user selects menu option 2}
+    3 { Commands to be run if user selects menu option 3}
+    4 { Commands to be run if user selects menu option 4}
+    }
+}
+While ($Select -ne 5)
+
+
